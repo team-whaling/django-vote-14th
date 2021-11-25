05dfd13fdc1210fcd5fe9da8429e614c476d390a
hwanu <sossont1@gmail.com>
2021-11-26 07:32:49 +0900

    [develop] Add .env.prod in .gitignore

--- vote/urls.py
+++ vote/urls.py
@@ -1,4 +1,4 @@
-"""vote2 URL Configuration
+"""vote URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- vote/wsgi.py
+++ vote/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vote2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vote.settings')
 
 application = get_wsgi_application()
