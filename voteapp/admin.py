8414cd38b966e2d241a482470ebb9617d85cf3fa
Min A <min2010011@gmail.com>
2021-11-27 23:20:35 +0900

    [develop] admin.py: 관리자 페이지 모델 추가

--- voteapp/admin.py
+++ voteapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'vote']
+
+# admin.site.register(User)
+# admin.site.register(Candidate)
