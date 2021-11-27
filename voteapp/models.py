58013e67f3b0398a11392eb74f44b157a78d0583
Min A <min2010011@gmail.com>
2021-11-27 22:58:22 +0900

    [develop] models.py: User와 Candidate model 추가

--- voteapp/models.py
+++ voteapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=128, unique=True)
+
+    class Meta:
+        db_table = 'user'
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=30)
+    vote = models.IntegerField()
+
+    class Meta:
+        db_table = 'candidate'
+        ordering = ['-vote']
