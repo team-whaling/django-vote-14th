7b395089b3fd8619e9a4fa4798d35bebdbeb31fc
Min A <min2010011@gmail.com>
2021-11-29 00:24:35 +0900

    [develop] Feat: Candidate View Router 추가

--- voteapp/urls.py
+++ voteapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import views
 from .views import *
 
 app_name = 'voteapp'   # 어플리케이션의 이름공간 설정.
 
+router = routers.DefaultRouter()
+router.register(r'candidates', CandidateViewSet)
+
 urlpatterns = [
     path('register/', Registartion.as_view()),
     path('login/', Login.as_view()),
+    path('', include(router.urls))
 ]
