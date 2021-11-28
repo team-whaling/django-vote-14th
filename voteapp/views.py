6cb1ca9cb87b0adcbdd0fdec8140283e283c6f1c
Min A <min2010011@gmail.com>
2021-11-28 23:08:34 +0900

    [develop] feat: candidate view ReadOnly로 변경

--- voteapp/views.py
+++ voteapp/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-from rest_framework import status,generics
-from django_filters.rest_framework import DjangoFilterBackend,filters,FilterSet
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import *
 from .serializers import CandidateSerializer
-# Create your views here.
 
 
 def index(request):
     return render(request, "voteapp/index.html")
 
 
-class CandidateViewSet(generics.ListAPIView):
+class CandidateViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CandidateSerializer
-    queryset = Candidate.object.all()
-    filter_backends = [DjangoFilterBackend]
-    permission_classes = [
-
-    ]
+    queryset = Candidate.objects.all()
 
