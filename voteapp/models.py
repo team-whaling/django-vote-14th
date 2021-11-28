7da14967b7e67624c6565450e5a793885cfd943a
Min A <min2010011@gmail.com>
2021-11-28 17:09:00 +0900

    [develop] models.py: User 모델에 투표 여부 필드 추가

--- voteapp/models.py
+++ voteapp/models.py
@@ -1,10 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(max_length=128, unique=True)
+    voted = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'user'
