e98db4ac30d95c5b0191e794981aac82860f9051
Hwanu Jung <sossont1@gmail.com>
2021-11-28 20:46:05 +0900

    [develop] Chore : add JWT in settings

--- voteapp/urls.py
+++ voteapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = 'voteapp'   # 어플리케이션의 이름공간 설정.
 ## 제너릭 뷰 사용하기.
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path(r'candidate', CandidateViewSet.as_view())
 ]
