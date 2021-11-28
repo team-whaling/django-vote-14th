5684ce75d758146bc1f86a84c571d6f71c284e8a
Min A <min2010011@gmail.com>
2021-11-29 02:00:15 +0900

    [develop] Refactor: reformat files

--- voteapp/serializers.py
+++ voteapp/serializers.py
@@ -1,21 +1,24 @@
-from rest_framework import serializers
-from .models import Candidate,User
-from rest_framework_jwt.settings import api_settings
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import update_last_login
-from django.contrib.auth import authenticate
-from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+
+from .models import Candidate, User
 
 JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
 JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
 
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
         fields = '__all__'
 
+
 # Login
 User = get_user_model()
+
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=20)
@@ -29,7 +32,7 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=username, password=password)
 
         if user is None:
-            return {'username' : 'None'}
+            return {'username': 'None'}
         try:
             payload = JWT_PAYLOAD_HANDLER(user)
             jwt_token = JWT_ENCODE_HANDLER(payload)
@@ -40,8 +43,8 @@ class UserLoginSerializer(serializers.Serializer):
                 "Given username and passowrd does not exist"
             )
         return {
-            'username' : user.username,
-            'token' : jwt_token
+            'username': user.username,
+            'token': jwt_token
         }
 
 
--- voteapp/views.py
+++ voteapp/views.py
@@ -1,24 +1,25 @@
-from rest_framework import status,generics, viewsets
-from rest_framework.response import Response
-from .serializers import *
-from rest_framework.permissions import AllowAny
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import status, generics, viewsets
 # Need For using JWT
-from rest_framework.decorators import permission_classes, action
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .serializers import *
 
 
 class Registartion(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
-    def post(self,request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if not serializer.is_valid(raise_exception=True):
-            return Response({"message" : "Register Request Body Error."}, status=status.HTTP_409_CONFLICT)
+            return Response({"message": "Register Request Body Error."}, status=status.HTTP_409_CONFLICT)
         serializer.is_valid(raise_exception=True)
         user = serializer.save(request)
         return Response({
-            "user" : UserSerializer(user, context=self.get_serializer_context()).data
+            "user": UserSerializer(user, context=self.get_serializer_context()).data
         },
-        status = status.HTTP_201_CREATED)
+            status=status.HTTP_201_CREATED)
 
 
 class Login(generics.GenericAPIView):
@@ -27,16 +28,16 @@ class Login(generics.GenericAPIView):
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         if not serializer.is_valid(raise_exception=True):
-            return Response({"message" : "Login Request Body Error."}, status=status.HTTP_409_CONFLICT)
+            return Response({"message": "Login Request Body Error."}, status=status.HTTP_409_CONFLICT)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
         if user['username'] == "None":
-            return Response({"message" : "Login Fail"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"message": "Login Fail"}, status=status.HTTP_401_UNAUTHORIZED)
 
         return Response(
             {
-                "user" : UserSerializer(user, context=self.get_serializer_context()).data,
-                "token" : user['token']
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "token": user['token']
             }
         )
 
